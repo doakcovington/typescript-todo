b328e15072c6176a06b918030973f55ec78ac37a
doakcovington <doakcovington@protonmail.com>
2020-10-23 14:21:48 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+//NCWH
+//WDGTR
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -8,16 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          We Don't Go To Ravenholm
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
