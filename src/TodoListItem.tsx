c7d6805152ded83b5b231fd59873ed17320a2500
doakcovington <doakcovington@protonmail.com>
2020-10-24 15:46:33 -0700

    Built out component

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+interface Props {
+  todo: Todo;
+  toggleTodo: ToggleTodo;
+}
 
-export const ToDoListItem =  () => {
+export const TodoListItem: React.FC<Props> =  ({ todo, toggleTodo }) => {
   return (
-    <li>Item</li>
+    <li>
+      <label style={{ textDecoration: todo.complete ? 'line-through' : undefined}}>
+        <input type='checkbox' checked={todo.complete} onClick={() => toggleTodo(todo)}/> {todo.text}
+      </label>
+    </li>
   )
 }
